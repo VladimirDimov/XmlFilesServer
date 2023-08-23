f8cb7cd2d2d8e0cbdd5083c3df9f117cd8bb2a1f
vladim <vladim@cobuilder.no>
2023-08-23 18:26:18 +0300

    initial

---
+++ src/XmlFileServer/Web/Models/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Models
+{
+    public class ResponseModel<T>
+    {
+        public ResponseModel(T content)
+        {
+            Content = content;
+        }
+
+        public bool IsSuccess { get; set; }
+
+        public T Content { get; set; }
+
+        public List<string> ValidationErrors { get; set; }
+    }
+}
